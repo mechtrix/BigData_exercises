be4da5c3ca5ec41668fd832cc40c7e08da283efa
mechtrix <weber.tim1985@gmail.com>
2024-10-07 19:26:00 +0200

    added plotting to csv files

--- analysis/csv_flights.R
+++ analysis/csv_flights.R
@@ -1,6 +1,7 @@
 library(here)
 library(tidyverse)
 library(readr)
+library(scales)
 
 flights_data_readr <-  read_csv(here("data","flights.csv"))
 
@@ -12,3 +13,27 @@ flights_data_readr |>
   )+
   geom_histogram()+
   theme_minimal()
+
+flights_data_readr |> 
+  ggplot(
+    aes(
+      x = dep_delay,
+    )
+  )+
+  geom_histogram(
+    bins = 20,
+    fill = "steelblue",
+    color = "white"
+  )+
+  labs(
+    title = "departure delay for origin airports",
+    x = "delay in min",
+    y = "count"
+  )+
+  facet_wrap(
+    ~origin
+    )+
+  scale_y_log10(
+    labels = label_log()
+    )+
+  theme_minimal(base_size = 15)
